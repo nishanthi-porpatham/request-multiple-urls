5895e0da960bf0c1392f779f0eeb2fb381fcaf4e
Nishanthi Porpatham <nisha.porpatham@outlook.com>
2020-10-23 00:35:33 +0100

    Added package-lock file and fixed url

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const requestMultipleUrls = require('./request-multiple-urls');
 const requestMultipleurls = require('./request-multiple-urls');
 const urls = [
-    'https://jsonplaceholder.typicode.com/postss/1',
+    'https://jsonplaceholder.typicode.com/posts/1',
     'https://jsonplaceholder.typicode.com/posts/1/comments',
     'https://jsonplaceholder.typicode.com/posts?userId=1'
     ];
